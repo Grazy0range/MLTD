9324c5f2e7f4a82b17019c5219ce8a9b7e0a59e0
Grazy0range <76399929+Grazy0range@users.noreply.github.com>
2023-01-23 20:26:06 +0300

    123

--- js/script.js
+++ js/script.js
@@ -7,3 +7,34 @@ $(document).ready(function () {
     });
 });
 
+function slowScroll(id) {
+  var offset = 0;
+  $("html, body").animate(
+    {
+      scrollTop: $(id).offset().top - offset,
+    },
+    500
+  );
+  return false;
+}
+
+function noperevod(events) {
+  events.preventDefault();
+  };
+
+jQuery(document).ready(function($) {
+  var
+    $window = $(window), 
+    $target = $(".header"),
+    $h = $target.offset().top; 
+  $window.on('scroll', function() {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > $h) {
+      $target.addClass("sheensay_fixed");
+    } else {     
+      $target.removeClass("sheensay_fixed");
+    }
+  });
+ 
+});
+
